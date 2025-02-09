547deb54b3570953cdfc592fba22cb507d3dda87
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-09 12:31:37 +0800

    feat- add credit_package and skill schema

--- week4/db.js
+++ week4/db.js
@@ -10,7 +10,52 @@ const CreditPackage = new EntitySchema({
       generated: "uuid",
       nullable: false,
     },
+    name: {
+      type: "varchar",
+      length: 50,
+      unique: true,
+      nullable: false
+    },
+    credit_amount: {
+      type: "int",
+      nullable: false
+    },
+    price: {
+      type: "numeric", 
+      precision: 10, 
+      scale: 2, 
+      nullable: false 
+    },
+    create_at: {
+      type: "timestamp",
+      default: ()=> "CURRENT_TIMESTAMP",
+      nullable: false
+    }
   },
+})
+
+const Skill = new EntitySchema({
+  name: "SKILL",
+  tableName: "SKILL",
+  columns: {
+    id: {
+      primary: true,
+      type: "uuid",
+      generated: "uuid",
+      nullable: false,
+    },
+    name: {
+      type: "varchar",
+      length: 50,
+      unique: true,
+      nullable: false
+    },
+    create_at: {
+      type: "timestamp",
+      default: ()=> "CURRENT_TIMESTAMP",
+      nullable: false
+    }
+  } 
 })
 
 const AppDataSource = new DataSource({
@@ -20,7 +65,7 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "test",
   database: process.env.DB_DATABASE || "test",
-  entities: [CreditPackage],
+  entities: [CreditPackage,Skill],
   synchronize: true,
 })
 
--- week4/server.js
+++ week4/server.js
@@ -1,17 +1,34 @@
 require("dotenv").config()
 const http = require("http")
 const AppDataSource = require("./db")
+const { Console } = require("console")
 
 const requestListener = async (req, res) => {
-  const headers = {}
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+   'Content-Type': 'application/json'
+ }
   let body = ""
   req.on("data", (chunk) => {
     body += chunk
   })
 
   if (req.url === "/api/credit-package" && req.method === "GET") {
-    
-  } else if (req.url === "/api/credit-package" && req.method === "POST") {
+    console.log('get api')
+    res.writeHead(200, headers)
+    res.write(JSON.stringify({
+      status: "success",
+      data: []
+    }))
+    res.end()
+  } 
+  // POST-新增課程購買方案
+  else if (req.url === "/api/credit-package" && req.method === "POST") {
+    req.on("end",()=>{
+
+    })
     
   } else if (req.url.startsWith("/api/credit-package/") && req.method === "DELETE") {
     
@@ -37,5 +54,6 @@ async function startServer() {
   console.log(`伺服器啟動成功, port: ${process.env.PORT}`)
   return server;
 }
+
 
 module.exports = startServer();
