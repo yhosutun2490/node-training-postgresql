679f5434e9d116342d3e257056c7f3a5c348a788
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-23 22:09:54 +0800

    feat- add delete(cancell) course api

--- week6/routes/courses.js
+++ week6/routes/courses.js
@@ -3,17 +3,18 @@ const express = require("express");
 const router = express.Router();
 const { dataSource } = require("../db/data-source");
 const logger = require("../utils/logger")("Courses");
+const { IsNull } = require("typeorm");
 const {
-    successResponse,
-    customErrorResponse,
-  } = require("../middlewares/responseHandler");
+  successResponse,
+  customErrorResponse,
+} = require("../middlewares/responseHandler");
 
 const {
-    isCourseIdExist,
-    isUserAlreadyBooked,
-    isUserRemainBookingCredits,
-    isOverCourseMaxParticipants
-} = require("../middlewares/courses/index")
+  isCourseIdExist,
+  isUserAlreadyBooked,
+  isUserRemainBookingCredits,
+  isOverCourseMaxParticipants,
+} = require("../middlewares/courses/index");
 
 const config = require("../config/index");
 const { userAuth } = require("../middlewares/auth");
@@ -26,45 +27,78 @@ const auth = userAuth({
 
 // 取得所有課程
 router.get("/", async (req, res, next) => {
-    try {
-      const courseResult = await dataSource.getRepository("Course").find({
-        select: [
-          "id",
-          "user_id",
-          "skill_id",
-          "name",
-          "description",
-          "start_at",
-          "end_at",
-          "max_participants",
-          "meeting_url",
-        ],
-      });
-      successResponse(res, courseResult, 200);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const courseResult = await dataSource.getRepository("Course").find({
+      select: [
+        "id",
+        "user_id",
+        "skill_id",
+        "name",
+        "description",
+        "start_at",
+        "end_at",
+        "max_participants",
+        "meeting_url",
+      ],
+    });
+    successResponse(res, courseResult, 200);
+  } catch (err) {
+    next(err);
+  }
+});
 // 報名課程
-router.post("/:courseId",auth,[
+router.post(
+  "/:courseId",
+  auth,
+  [
     isCourseIdExist,
     isUserAlreadyBooked,
     isUserRemainBookingCredits,
-    isOverCourseMaxParticipants
-] ,async(req, res, next)=>{
-    const userId = req.user.id
-    const courseId = req.params.courseId
+    isOverCourseMaxParticipants,
+  ],
+  async (req, res, next) => {
+    const userId = req.user.id;
+    const courseId = req.params.courseId;
     try {
-        const bookingTable = dataSource.getRepository("COURSE_BOOKING") 
-        const newBooking = bookingTable.create({
-            user_id: userId,
-            course_id: courseId
-        })
-        await bookingTable.save(newBooking)
-        successResponse(res,null,200)
-    } catch(err) {
-        next(err)
+      const bookingTable = dataSource.getRepository("COURSE_BOOKING");
+      const newBooking = bookingTable.create({
+        user_id: userId,
+        course_id: courseId,
+      });
+      await bookingTable.save(newBooking);
+      successResponse(res, null, 200);
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
-module.exports = router
+// 刪除課程報名 軟刪除 加入取消時間
+router.delete("/:courseId", auth, [isCourseIdExist], async (req, res, next) => {
+  const userId = req.user.id;
+  const courseId = req.params.courseId;
+  try {
+    const bookingTable = dataSource.getRepository("COURSE_BOOKING");
+    // update 條件->資料
+    const updateResult = await bookingTable.update(
+      {
+        user_id: userId,
+        course_id: courseId,
+        cancelledAt: IsNull(),
+      },
+      {
+        cancelledAt: new Date().toISOString(),
+      }
+    );
+
+    if (updateResult.affected === 0) {
+      customErrorResponse(res, 400, "取消失敗");
+      return;
+    }
+    successResponse(res, null, 200);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
