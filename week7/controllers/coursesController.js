6afaf244e31db566a7c4fea4cdec74990ef0a239
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-28 17:50:21 +0800

    refactor- refactor courses controller

---
+++ week7/controllers/coursesController.js
@@ -0,0 +1,61 @@
+const { dataSource } = require("../db/data-source");
+const logger = require("../utils/logger")("AdminCoachController");
+const catchAsync = require("../utils/catchAsync");
+
+const courses = {
+  get: catchAsync(async (req, res, next) => {
+    const courseResult = await dataSource.getRepository("Course").find({
+      select: [
+        "id",
+        "user_id",
+        "skill_id",
+        "name",
+        "description",
+        "start_at",
+        "end_at",
+        "max_participants",
+        "meeting_url",
+      ],
+    });
+    successResponse(res, courseResult, 200);
+  }),
+  post: catchAsync(async (req, res, next) => {
+    const userId = req.user.id;
+    const courseId = req.params.courseId;
+
+    const bookingTable = dataSource.getRepository("COURSE_BOOKING");
+    const newBooking = bookingTable.create({
+      user_id: userId,
+      course_id: courseId,
+    });
+    await bookingTable.save(newBooking);
+    successResponse(res, null, 200);
+  }),
+  delete: catchAsync(async (req, res, next) => {
+    const userId = req.user.id;
+    const courseId = req.params.courseId;
+
+    const bookingTable = dataSource.getRepository("COURSE_BOOKING");
+    // update 條件->資料
+    const updateResult = await bookingTable.update(
+      {
+        user_id: userId,
+        course_id: courseId,
+        cancelledAt: IsNull(),
+      },
+      {
+        cancelledAt: new Date().toISOString(),
+      }
+    );
+
+    if (updateResult.affected === 0) {
+      customErrorResponse(res, 400, "取消失敗");
+      return;
+    }
+    successResponse(res, null, 200);
+  }),
+};
+
+module.exports = {
+  courses,
+};
--- week7/routes/courses.js
+++ week7/routes/courses.js
@@ -16,6 +16,8 @@ const {
   isOverCourseMaxParticipants,
 } = require("../middlewares/courses/index");
 
+const { courses } = require("../controllers/coursesController")
+
 const config = require("../config/index");
 const { userAuth } = require("../middlewares/auth");
 // userAuth init
@@ -26,26 +28,7 @@ const auth = userAuth({
 });
 
 // 取得所有課程
-router.get("/", async (req, res, next) => {
-  try {
-    const courseResult = await dataSource.getRepository("Course").find({
-      select: [
-        "id",
-        "user_id",
-        "skill_id",
-        "name",
-        "description",
-        "start_at",
-        "end_at",
-        "max_participants",
-        "meeting_url",
-      ],
-    });
-    successResponse(res, courseResult, 200);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", courses.get);
 // 報名課程
 router.post(
   "/:courseId",
@@ -56,49 +39,10 @@ router.post(
     isUserRemainBookingCredits,
     isOverCourseMaxParticipants,
   ],
-  async (req, res, next) => {
-    const userId = req.user.id;
-    const courseId = req.params.courseId;
-    try {
-      const bookingTable = dataSource.getRepository("COURSE_BOOKING");
-      const newBooking = bookingTable.create({
-        user_id: userId,
-        course_id: courseId,
-      });
-      await bookingTable.save(newBooking);
-      successResponse(res, null, 200);
-    } catch (err) {
-      next(err);
-    }
-  }
+  courses.post
 );
 
 // 刪除課程報名 軟刪除 加入取消時間
-router.delete("/:courseId", auth, [isCourseIdExist], async (req, res, next) => {
-  const userId = req.user.id;
-  const courseId = req.params.courseId;
-  try {
-    const bookingTable = dataSource.getRepository("COURSE_BOOKING");
-    // update 條件->資料
-    const updateResult = await bookingTable.update(
-      {
-        user_id: userId,
-        course_id: courseId,
-        cancelledAt: IsNull(),
-      },
-      {
-        cancelledAt: new Date().toISOString(),
-      }
-    );
-
-    if (updateResult.affected === 0) {
-      customErrorResponse(res, 400, "取消失敗");
-      return;
-    }
-    successResponse(res, null, 200);
-  } catch (err) {
-    next(err);
-  }
-});
+router.delete("/:courseId", auth, [isCourseIdExist], courses.delete);
 
 module.exports = router;
