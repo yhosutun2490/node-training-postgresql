2f1398a4642fff4ff38512100acff93ad82ed92b
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-28 15:38:50 +0800

    refactor- refactor users route to usersController

--- week7/controllers/usersController.js
+++ week7/controllers/usersController.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const { dataSource } = require("../db/data-source");
 const logger = require("../utils/logger")("UsersController");
-const catchAsync = require(".../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync");
 const {
   successResponse,
   customErrorResponse,
--- week7/routes/users.js
+++ week7/routes/users.js
@@ -12,18 +12,14 @@ const {
   isEmailOrNameRepeat,
   isUserExist,
   isUpdateSameName,
-  isDBhasSameName
+  isDBhasSameName,
 } = require("../middlewares/users/index");
+
+// controller
+const { signup, login, profile } = require("../controllers/usersController");
+
 const { userAuth } = require("../middlewares/auth");
-
-const {
-  successResponse,
-  customErrorResponse,
-} = require("../middlewares/responseHandler");
-const { hashPassword } = require("../utils/bcryptPassword");
-const { generateJwtToken } = require("../utils/generateJWTtoken");
 const config = require("../config/index");
-
 // userAuth init
 const auth = userAuth({
   secret: config.get("secret").jwtSecret,
@@ -31,115 +27,17 @@ const auth = userAuth({
   logger,
 });
 
-router.post(
-  "/signup",
-  [userSignUpValidator, isEmailOrNameRepeat],
-  async (req, res, next) => {
-    try {
-      const { name, email, password, role } = req.body;
-      const userTable = await dataSource.getRepository("User");
-      const passwordHashed = await hashPassword(password);
-      const createUser = userTable.create({
-        name,
-        email,
-        password: passwordHashed,
-        role,
-      });
-      const result = await userTable.save(createUser);
-      logger.info("新建立的使用者ID:", result.id);
-      successResponse(
-        res,
-        {
-          user: {
-            id: result.id,
-            name,
-          },
-        },
-        201
-      );
-    } catch (err) {
-      logger.error("創建user錯誤:", err);
-      next(err);
-    }
-  }
-);
+router.post("/signup", [userSignUpValidator, isEmailOrNameRepeat], signup.post);
 
-router.post(
-  "/login",
-  [userLoginValidator, isUserExist],
-  async (req, res, next) => {
-    try {
-      const token = await generateJwtToken(
-        {
-          id: req.id,
-          role: req.role,
-        },
-        config.get("secret.jwtSecret"),
-        {
-          expiresIn: `${config.get("secret.jwtExpiresDay")}`,
-        }
-      );
-      successResponse(
-        res,
-        {
-          token,
-          user: {
-            name: req.name,
-          },
-        },
-        200
-      );
-    } catch (err) {
-      logger.error("登入錯誤:", err);
-      next(err);
-    }
-  }
-);
+router.post("/login", [userLoginValidator, isUserExist], login.post);
 
 // 取得個人資料
-router.get("/profile", auth, async (req, res, next) => {
-  try {
-    const { id } = req.user;
-    const userTable = dataSource.getRepository("User");
-    const user = await userTable.findOne({
-      select: ["name", "email"],
-      where: { id },
-    });
-    successResponse(res, { data: user });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/profile", auth, profile.get);
 
 router.put(
   "/profile",
-  [auth,userUpdateProfileValidator,isUpdateSameName,isDBhasSameName],
-  async (req, res, next) => {
-    try {
-      const userTable = dataSource.getRepository("User");
-
-      const updateResult = await userTable.update(
-        {id: req.user.id},
-        {name: req.body.name},
-      );
-      if (updateResult.affected === 0) {
-        customErrorResponse(req, 400, "更新使用者資料失敗");
-        return;
-      } else {
-        const result = await userTable.findOne({
-          select: ["name"],
-          where: {
-            id: req.user.id,
-          },
-        });
-        successResponse(res, {
-          data: result
-        }, 200)
-      }
-    } catch (err) {
-      next(err);
-    }
-  }
+  [auth, userUpdateProfileValidator, isUpdateSameName, isDBhasSameName],
+  profile.put
 );
 
 module.exports = router;
