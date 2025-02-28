55224a9bf9957da12d043333f8234721ccf8f084
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-28 20:55:59 +0800

    fix- fix update course body validator

--- week6/middlewares/admin/validateAdmin.js
+++ week6/middlewares/admin/validateAdmin.js
@@ -93,15 +93,6 @@ function updateCourseValidate(req, res, next) {
         /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
         "skill_id必須是有效格式"
       ),
-    user_id: z
-      .string({
-        invalid_type_error: "user_id必須是字串",
-      })
-      .nonempty("user_id不能為空")
-      .regex(
-        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
-        "user_id必須是有效格式"
-      ),
     skill_id: z
       .string({
         invalid_type_error: "skill_id必須是字串",
