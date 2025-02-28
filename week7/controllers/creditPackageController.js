09552061a7ed14b2555f799b1c4a995c2e4c02bb
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-28 18:10:14 +0800

    refacotr-m refacotr creditPackage controller

---
+++ week7/controllers/creditPackageController.js
@@ -0,0 +1,19 @@
+const { dataSource } = require("../db/data-source");
+const logger = require("../utils/logger")("AdminCoachController");
+const catchAsync = require("../utils/catchAsync");
+
+const creditPackage = {
+  get: catchAsync(async (req, res, next) => {
+    const data = await dataSource.getRepository("CREDIT_PACKAGE").find({
+      select: ["id", "name", "credit_amount", "price"],
+    });
+    successResponse(res, data);
+  }),
+  createPackage: catchAsync(),
+  buyPackage: catchAsync(),
+  delete: catchAsync(),
+};
+
+module.exports = {
+  creditPackage,
+};
