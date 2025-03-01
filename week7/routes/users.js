c803d01c5205e47883ad8bbb22838034cd3d3c67
yhosutun2491 <yhosutun2491@gmail.com>
2025-03-01 13:07:15 +0800

    feat- update password limit 3 times per minute

--- week7/routes/users.js
+++ week7/routes/users.js
@@ -33,6 +33,15 @@ const auth = userAuth({
   repository: dataSource.getRepository("User"),
   logger,
 });
+// limit update password request
+const  { rateLimit } = require('express-rate-limit')
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 15 minutes
+	limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
 router.post("/signup", [userSignUpValidator, isEmailOrNameRepeat], signup.post);
 
@@ -52,6 +61,7 @@ router.put(
 router.put(
   "/password",
   auth,
+  limiter,
   [userUpdatePasswordValidator, isUserInputPasswordMatchDb],
   passwordController.put
 );
