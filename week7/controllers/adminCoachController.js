e4c22283813f15873a5f05d19306dd6ad6406c14
yhosutun2491 <yhosutun2491@gmail.com>
2025-03-02 12:10:58 +0800

    fix- admin coach get personal data left join

--- week7/controllers/adminCoachController.js
+++ week7/controllers/adminCoachController.js
@@ -10,16 +10,29 @@ const adminCoach = {
   get: catchAsync(async (req, res,next) => {
     // 取得教練自己的詳細資料 get coach data by id
     // inner join User , Coach Table and skill table (coach skills ids)
-    // 需要修正新增或更新教練資料時 skill link關聯才能正確
+    // 需要更新教練skills資料時 skill link關聯才能正確，需要left join確保尚未新增skill，保留資料正確
     const userId = req.user.id
     const userTable = dataSource.getRepository('User')
     const coachResult  = await userTable.createQueryBuilder('user')
     .innerJoinAndSelect('Coach','coach',"user.id = coach.user_id")
-    .innerJoinAndSelect('CoachLinkSkill','skill_link',"skill_link.coach_id = coach.id")
-    .innerJoinAndSelect('CoachSkill','skill',"skill.id = skill_link.skill_id")
+    .leftJoinAndSelect('CoachLinkSkill','skill_link',"skill_link.coach_id = coach.id")
+    .leftJoinAndSelect('CoachSkill','skill',"skill.id = skill_link.skill_id")
     .where("user.id = :id", {id: userId})
+    .select(
+      [
+      "coach.id AS id",
+			"coach.experience_years AS experience_years" ,
+			"coach.description AS description",
+			"coach.profile_image_url AS profile_image_url",
+			"skill.id AS skill_ids" 
+      ]
+    )
     .getRawMany()
     if (coachResult?.length) {
+      // skill id null -> empty array
+      if (!coachResult[0].skill_ids) {
+        coachResult[0].skill_ids = []
+      }
       successResponse(res,coachResult[0],200)
       return
     } else {
