0108d07656d4141e57ac70b08ab5fbf33678ecb7
yhosutun2491 <yhosutun2491@gmail.com>
2025-03-04 22:24:05 +0800

    refactor- skills api add auth and isCoach middlewar

--- week7/routes/coachSkill.js
+++ week7/routes/coachSkill.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../utils/logger")("CoachSkill");
+const { dataSource } = require("../db/data-source");
 const {
   createCoachesSkillValidator,
   deleteSkillValidator,
@@ -8,15 +9,27 @@ const {
 const {
   hasSameSkillName
 } = require("../middlewares/coachSkill/index")
+
+const {
+  isCoach,
+} = require("../middlewares/admin/index");
 const {
   coachSkill
 } = require("../controllers/coachSkill")
+const config = require("../config/index");
+const { userAuth } = require("../middlewares/auth");
 
+// userAuth init
+const auth = userAuth({
+  secret: config.get("secret").jwtSecret,
+  repository: dataSource.getRepository("User"),
+  logger,
+});
 
-router.get("/", coachSkill.get);
+router.get("/", auth ,isCoach, coachSkill.get);
 
-router.post("/", [createCoachesSkillValidator,hasSameSkillName], coachSkill.post);
+router.post("/",auth, isCoach, [createCoachesSkillValidator,hasSameSkillName], coachSkill.post);
 
-router.delete("/:id", deleteSkillValidator, coachSkill.delete);
+router.delete("/:id",auth, isCoach ,deleteSkillValidator, coachSkill.delete);
 
 module.exports = router;
