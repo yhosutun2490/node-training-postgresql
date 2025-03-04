972b934eae7bdcf09e4469d7edcef2d51ad175a6
yhosutun2490 <yhosutun2491@gmail.com>
2025-03-04 09:05:13 +0800

    fix- close upload api env problem

--- week7/config/secret.js
+++ week7/config/secret.js
@@ -2,7 +2,7 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET,
     jwtExpiresDay: process.env.JWT_EXPIRES_DAY,
     firebase: {
-      serviceAccount: JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT),
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+      serviceAccount: JSON.parse(process.env?.FIREBASE_SERVICE_ACCOUNT) ,
+      storageBucket: process.env?.FIREBASE_STORAGE_BUCKET  
     }
   }
